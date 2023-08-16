2d904d0bfc8ce1f7853a35a9954fb70ac4d5af36
Krishna-Kondepati <krishna.kondepati@teliacompany.com>
2023-08-16 08:54:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,29 +4,7 @@ const tagPrefix = `${process.env.INPUT_PREFIX || ''}*`;
 
 exec(`git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1)`, (err, tag, stderr) => {
     tag = tag.trim();
-    fs.appendFileSync(process.env.GITHUB_OUTPUT, `tag=${tag}\n`);
+    //fs.appendFileSync(process.env.GITHUB_OUTPUT, `tag=${tag}\n`);
     console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
     process.exit(0);
-
-    if (err) {
-        console.log('\x1b[33m%s\x1b[0m', 'Could not find any tags because: ');
-        console.log('\x1b[31m%s\x1b[0m', stderr);
-        process.exit(1);
-    }
-
-    exec(`git log -1 --format=%at ${tag}`, (err, timestamp, stderr) => {
-        if (err) {
-            console.log('\x1b[33m%s\x1b[0m', 'Could not find any timestamp because: ');
-            console.log('\x1b[31m%s\x1b[0m', stderr);
-            process.exit(1);
-        }
-
-        timestamp = timestamp.trim();
-
-        console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
-        console.log('\x1b[32m%s\x1b[0m', `Found timestamp: ${timestamp}`);
-        fs.appendFileSync(process.env.GITHUB_OUTPUT, `tag=${tag}\n`);
-        fs.appendFileSync(process.env.GITHUB_OUTPUT, `timestamp=${timestamp}\n`);
-        process.exit(0);
-    });
 });
