14c14413d74b70271381cd99bc575cb4df15e0f8
Krishna-Kondepati <krishna.kondepati@teliacompany.com>
2023-08-16 10:41:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const tagPrefix = `${process.env.INPUT_PREFIX || ''}*`;
 
 exec(`git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1)`, (err, tag, stderr) => {
     tag = tag.trim();
-    fs.appendFileSync(process.env.GITHUB_OUTPUT, `tag=${process.env.INPUT_FALLBACK}\n`);
+    console.log(process.env);
+    fs.appendFileSync(process.env.GITHUB_OUTPUT, `tag=${tag}\n`);
     console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
     process.exit(0);
 });
